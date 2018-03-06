cb210db48051e199ef9d0632d9c334972cb6cf19
vanyadeg <id1997@yandex.ru>
2018-03-06 10:29:20 +0300

    HW3

--- hw3/sigusr.c
+++ hw3/sigusr.c
@@ -5,18 +5,52 @@
 #include <stdio.h>
 #include <signal.h>
 #include <string.h>
+#include <errno.h>
+#include <zconf.h>
 
-void handler(int sig, siginfo_t *siginfo,  void* _) {
-    //TODO:
+int caught = 0;
+int is_alarm = 0;
+pid_t cpid;
+
+void handler(int sig, siginfo_t *siginfo, void *_) {
+    if (sig == SIGALRM) {
+        is_alarm = 1;
+        return;
+    }
+    if (caught == 0) {
+        caught = sig;
+        cpid = siginfo->si_pid;
+    }
 }
 
 int main() {
     struct sigaction act;
-    memset(&act, 0, sizeof(act));
     act.sa_flags = SA_SIGINFO;
     act.sa_sigaction = &handler;
 
-    //TODO:
+    if (sigemptyset(&act.sa_mask)) {
+        fprintf(stderr, "Error during sigemptyset(): %s\n", strerror(errno));
+    }
+    if (sigaddset(&act.sa_mask, SIGALRM) ||
+        sigaddset(&act.sa_mask, SIGUSR1) ||
+        sigaddset(&act.sa_mask, SIGUSR2)) {
+        fprintf(stderr, "Error during sigaddset(): %s\n", strerror(errno));
+    }
+    if (sigaction(SIGALRM, &act, NULL) ||
+        sigaction(SIGUSR1, &act, NULL) ||
+        sigaction(SIGUSR2, &act, NULL)) {
+        fprintf(stderr, "Error during sigaction(): %s\n", strerror(errno));
+    }
 
+    alarm(10);
+    while (is_alarm == 0) {
+        if (caught != 0) {
+            printf("%s from %d\n", caught == SIGUSR1 ? "SIGUSR1" : "SIGUSR2", cpid);
+            return 0;
+        }
+        pause();
+    }
+
+    printf("No signals were caught\n");
     return 0;
 }
